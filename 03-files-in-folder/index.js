448a593c8cf0eed3a946eccf745191e0c88fcbc9
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2024-01-23 10:21:30 +0300

    refactor: add async/await to 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,23 @@
-const { readdir, stat } = require('fs');
+const fs = require('fs');
+const { readdir } = require('fs/promises');
 const { join, parse, extname } = require('path');
 const { stdout } = require('process');
 const { EOL } = require('os');
 
 const dirPath = join(__dirname, 'secret-folder');
 
-readdir(dirPath, { withFileTypes: true }, (err, files) => {
-  if (err) console.log(err);
+const getFilesInfo = async (src) => {
+  const files = await readdir(dirPath, { withFileTypes: true });
   files
     .filter((file) => file.isFile())
-    .forEach((file) => {
-      const filePath = join(dirPath, file.name);
+    .forEach(async (file) => {
+      const filePath = join(src, file.name);
       const fileName = parse(filePath).name;
       const fileExtension = extname(filePath).slice(1);
-      stat(filePath, (err, stats) => {
-        if (err) console.log(err);
-        const fileSize = `${(stats.size / 1024).toFixed(3)}kb`;
-        stdout.write(`${fileName} - ${fileExtension} - ${fileSize}${EOL}`);
-      });
+      const stat = await fs.promises.stat(filePath);
+      const fileSize = `${(stat.size / 1024).toFixed(3)}kb`;
+      stdout.write(`${fileName} - ${fileExtension} - ${fileSize}${EOL}`);
     });
-});
+};
+
+getFilesInfo(dirPath);
