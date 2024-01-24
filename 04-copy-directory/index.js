e982dda25a454316dc9a2744bb59b6597982d8e2
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2024-01-24 19:25:06 +0300

    feat: add solution 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const { exit } = require('process');
+const { stdout } = require('process');
+
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+fs.mkdir(destDir, { recursive: true }, (err) => {
+  if (err) {
+    console.log(err);
+    exit();
+  }
+});
+
+fs.readdir(srcDir, (err, files) => {
+  if (err) {
+    console.log(err);
+    exit();
+  }
+  files.forEach((file) => {
+    const filePath = path.join(srcDir, file);
+    const copiedFilePath = path.join(destDir, file);
+    fs.copyFile(filePath, copiedFilePath, (err) => {
+      if (err) {
+        console.log(err);
+        exit();
+      }
+    });
+  });
+  stdout.write('Copied!');
+});
